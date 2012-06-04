5ddcdca7ed9e6669f6de7fda487333ad705beee7
Jonathan Maltz <jm1271@gmail.com>
2012-06-04 18:05:48 -0400

    fixed the schema to account for the wordpress table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 04, 2012 at 11:11 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE `associations` (
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ureddit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `associations`
+--
+
+CREATE TABLE IF NOT EXISTS `associations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) NOT NULL,
   `type` varchar(32) NOT NULL,
@@ -10,9 +35,15 @@ CREATE TABLE `associations` (
   `creation` datetime NOT NULL,
   `modification` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE `association_attributes` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `association_attributes`
+--
+
+CREATE TABLE IF NOT EXISTS `association_attributes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `association_id` int(11) NOT NULL,
   `type` varchar(64) NOT NULL,
@@ -21,18 +52,30 @@ CREATE TABLE `association_attributes` (
   `creation` datetime NOT NULL,
   `modification` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE `logins` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logins`
+--
+
+CREATE TABLE IF NOT EXISTS `logins` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(64) NOT NULL,
   `contact_email` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE `objects` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `objects`
+--
+
+CREATE TABLE IF NOT EXISTS `objects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(32) NOT NULL,
   `value` varchar(255) NOT NULL,
@@ -40,9 +83,22 @@ CREATE TABLE `objects` (
   `creation` datetime NOT NULL,
   `modification` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
-CREATE TABLE `object_attributes` (
+--
+-- Dumping data for table `objects`
+--
+
+INSERT INTO `objects` (`id`, `type`, `value`, `ring`, `creation`, `modification`) VALUES
+(1, 'Catalog', 'Catalog', 1, '2012-06-04 00:00:00', '2012-06-04 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `object_attributes`
+--
+
+CREATE TABLE IF NOT EXISTS `object_attributes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL,
   `type` varchar(64) NOT NULL,
@@ -51,5 +107,24 @@ CREATE TABLE `object_attributes` (
   `creation` datetime NOT NULL,
   `modification` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_posts`
+--
+
+CREATE TABLE IF NOT EXISTS `wp_posts` (
+  `post_title` varchar(255) NOT NULL,
+  `post_name` varchar(255) NOT NULL,
+  `post_date` varchar(255) NOT NULL,
+  `post_status` varchar(255) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `post_type` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
